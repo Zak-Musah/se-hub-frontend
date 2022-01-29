f4ec29a24648593be9a94691f2a76715ea9eaa4a
Abdul-Razak <arazak4r@gmail.com>
2022-01-29 18:42:59 +0100

    added deployed url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app
       server.use(
         "/api",
         createProxyMiddleware({
-          target: "http://localhost:8000",
+          target: "https://se-hub-backend.herokuapp.com",
           changeOrigin: true,
         }),
       );
@@ -27,7 +27,7 @@ app
 
     server.listen(2000, (err) => {
       if (err) throw err;
-      console.log("> Ready on http://localhost:8000");
+      console.log("> Ready on https://se-hub-backend.herokuapp.com");
     });
   })
   .catch((err) => {
