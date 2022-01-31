795e5da6e02d5cf77e3503116bedb45c03b5e4a5
Abdul-Razak <arazak4r@gmail.com>
2022-01-31 21:22:35 +0100

    business details structure layed out

--- components/Business/BusinessDetais.tsx
+++ components/Business/BusinessDetais.tsx
@@ -1,79 +1,102 @@
 import React from "react";
+import styles from "../../styles/businessDetails.module.scss";
+import MemberDetails from "./MemberDetails";
 
 const BusinessDetails = ({ title }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim inventore
-        dolore laboriosam dicta ab iure voluptatibus, laborum reprehenderit
-        velit beatae provident eveniet praesentium voluptas tenetur debitis a
-        qui nihil veniam! Fugit molestiae enim minima voluptatibus eligendi
-        autem error fugiat. Accusantium amet aliquid, culpa fugiat ipsum
-        incidunt magnam libero delectus rerum corrupti! Molestiae sit, saepe rem
-        dolores veritatis cupiditate distinctio soluta. Odio maiores assumenda
-        facere provident culpa eveniet, suscipit expedita molestias magni
-        asperiores, fugit error non ducimus nihil unde accusamus officiis
-        inventore aspernatur! Facere, doloremque earum quisquam obcaecati
-        praesentium illum mollitia. Consectetur accusantium nemo assumenda
-        reprehenderit mollitia ullam doloribus cupiditate voluptas sequi
-        perferendis, veritatis quidem, amet libero sunt minus quisquam modi
-        cumque ipsam provident ab repudiandae maxime est a? Dolorem, sequi. Ipsa
-        commodi quaerat at aliquid dolorum quisquam aperiam ut perspiciatis hic
-        quasi veritatis, temporibus in voluptatum deserunt exercitationem
-        doloribus nisi. In doloremque architecto sapiente dolorem exercitationem
-        eius laborum vel. Error. A ex totam maiores assumenda earum voluptate
-        quae dolorem perspiciatis nostrum fugiat blanditiis quasi eveniet,
-        provident voluptatem soluta non. Voluptate magni ratione minus dolor
-        quisquam nam veritatis, hic sint a. Quaerat inventore error, ipsa
-        impedit vitae at. Molestiae sapiente nobis magni repellendus fugit qui
-        quo voluptate repudiandae voluptatum consequatur nesciunt sint placeat
-        obcaecati id delectus adipisci perspiciatis, modi enim accusantium! Aut
-        eaque ipsum ab expedita quidem, blanditiis ad odio quaerat ipsam
-        reprehenderit quos, earum saepe autem quibusdam culpa tempore.
-        Blanditiis minus voluptas voluptates laboriosam sunt deleniti porro,
-        iure consequatur qui! Officia quia nihil vel dolore dignissimos,
-        recusandae repellendus adipisci fugiat? Repellat dolores amet accusamus
-        soluta dolorem, necessitatibus perspiciatis earum laudantium nemo
-        laborum, facilis incidunt praesentium asperiores quae, nihil autem eius.
-        Error vel ad at nostrum maxime eaque vero, sapiente possimus eum!
-        Perferendis perspiciatis, repellat voluptas tempore iusto iure atque ex
-        animi doloremque ratione. Consequuntur sunt ex, porro voluptate ratione
-        repellat. Ipsa, impedit voluptatum. Iusto, ducimus? Eius similique
-        perspiciatis molestias, officiis totam in, corporis quidem repellat
-        beatae placeat debitis, voluptatem doloribus voluptate autem.
-        Reiciendis, illo harum eaque numquam praesentium accusantium eius. Quo
-        nesciunt repellat aperiam illum distinctio quisquam aspernatur sit cum
-        repudiandae tenetur aliquid hic dolores magnam, qui rem totam laboriosam
-        ratione iure sint earum adipisci soluta quod? Blanditiis, cum ea. Est
-        omnis, deserunt excepturi optio sunt culpa doloribus itaque veniam quo
-        voluptas accusantium consequuntur provident quidem perspiciatis
-        asperiores reprehenderit iusto enim voluptatum distinctio expedita
-        delectus, eum vero cupiditate! Pariatur, quaerat. Tempore consequatur
-        sit recusandae impedit, esse repudiandae sint repellat animi illum harum
-        id dicta odit in, cum similique cupiditate modi ea quos, rerum
-        inventore. Quibusdam est nostrum accusantium excepturi blanditiis! Odit
-        minus deleniti atque sequi dignissimos unde, dolores distinctio!
-        Pariatur error officia ab sequi, asperiores iure consequuntur est
-        dignissimos neque molestiae ipsam beatae itaque reprehenderit ex
-        recusandae reiciendis quibusdam unde? Libero alias molestiae vel quod
-        excepturi quaerat voluptates corporis exercitationem incidunt, quisquam
-        illum dolor quia in, sed sapiente necessitatibus iste, vitae eum
-        repudiandae consequatur? Optio perspiciatis quaerat nam debitis harum!
-        Sequi eos quisquam ipsam magni eveniet consectetur? Iste fugiat
-        veritatis architecto quo iure numquam, quam delectus sequi unde qui?
-        Aliquam voluptas incidunt nesciunt fugit quo! Itaque sequi eveniet
-        consequuntur sunt. Nesciunt officiis sapiente maxime voluptate enim
-        provident perspiciatis maiores minus, corrupti, quis explicabo velit.
-        Officiis, veritatis harum autem neque alias ducimus ipsum repellat
-        labore. Maxime saepe mollitia nisi dolor ducimus. Eum dolorem similique
-        ipsum laboriosam repellendus sequi velit. Incidunt consectetur quos sed
-        earum quia cumque debitis obcaecati voluptatum maiores dolorem
-        recusandae, velit similique totam assumenda accusantium atque excepturi
-        quisquam est. Odit vitae recusandae eum quis aspernatur! Iure, quo!
-        Eius, placeat. Possimus nostrum aliquam eos voluptatem natus, eaque
-        ipsam non omnis beatae suscipit quam earum consequatur, maxime ullam
-        obcaecati accusamus modi?
+    <div className="container d-flex flex-column">
+      <div className={`d-flex ${styles.header}`}>
+        <img
+          style={{
+            borderRadius: "50%",
+            objectFit: "cover",
+            marginRight: "20px",
+          }}
+          alt="logo"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          width={60}
+          height={60}
+        />
+        <h1 className="">{title}</h1>
+      </div>
+      <div className={`d-flex justify-space-between  ${styles.team}`}>
+        {/* <div> */}
+        <h1 className={`d-flex ${styles.teamTitle}`}>The TEAM</h1>
+        <div className={`d-flex ${styles.teamMemberDetails}`}>
+          <MemberDetails />
+          {/* <MemberDetails />
+
+          <MemberDetails />
+          <MemberDetails /> */}
+        </div>
+      </div>
+      <div
+        className={`container d-flex justify-content-between mt-5  ${styles.details}`}
+      >
+        <div
+          className={`d-flex flex-column w-50 p-3  lead  ${styles.detailsDescription}`}
+        >
+          <h3>WHAT WE DO</h3>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
+            dicta debitis, doloremque blanditiis, aperiam quis quod dolor alias
+            esse deserunt nesciunt ratione vel illo similique cum, neque
+            quisquam corporis totam!
+          </div>
+        </div>
+        <div
+          className={`d-flex flex-column w-50 p-3  lead  ${styles.detailsContact}`}
+        >
+          <h3>Contact</h3>
+          <span>
+            <small> Phone:</small> {`0000000`}
+          </span>
+          <span>Email: aallalls@gmailcom</span>
+          <span>Website: wwdkkkkkkkkkkkkkkkkkkkkkkkkkklewll</span>
+          <span>Address: ddjjdjjjjjjjjjjjjjjjjjjjjjjdjd</span>
+        </div>
+      </div>
+      <div className="container">
+        <img
+          style={{
+            borderRadius: "10px",
+            margin: "20px",
+          }}
+          alt="logo"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          width={280}
+          height={280}
+        />
+        <img
+          style={{
+            borderRadius: "10px",
+            margin: "20px",
+          }}
+          alt="logo"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          width={280}
+          height={280}
+        />
+        <img
+          style={{
+            borderRadius: "10px",
+            margin: "20px",
+          }}
+          alt="logo"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          width={280}
+          height={280}
+        />
+        <img
+          style={{
+            borderRadius: "10px",
+            margin: "20px",
+          }}
+          alt="logo"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          width={280}
+          height={280}
+        />
       </div>
     </div>
   );
