25ac1c5858a5c70fade5768ad8671d9f91def3a4
ebgyau <ebgyau@outlook.com>
2022-02-12 20:28:01 +0100

    moved uploadform into a modal

--- components/forum/ask-question.tsx
+++ components/forum/ask-question.tsx
@@ -1,22 +1,45 @@
-import React from "react";
-import { Button } from 'antd';
-import UploadForum from "./upload-forum";
+import React , { useState } from "react";
+import { Button, Form, Input, Modal } from "antd";
 
 
 const AskQuestion = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
   return (
-      <div>
-        <Button type="primary" shape="round" onClick={uploadForm}> Ask Question </Button>
-      </div>
-    );
+    <>
+      <Button type="primary" onClick={showModal}>
+        Ask Question
+      </Button>
+      <Modal title="Ask Question" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <form {...layout} name="nest-messages">
+                <Form.Item label="Title" name="title" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item label="Description" name="description" rules={[{ required: true }]}>
+                    <Input.TextArea/>
+                </Form.Item>
+
+            </form>
+      </Modal>
+    </>
+  );
   };
   
   export default AskQuestion;
-
-function uploadForm(){
-  return(
-    <div className="container">
-      <UploadForum/>
-    </div>
-  )
-}
--- pages/forums/index.tsx
+++ pages/forums/index.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import AskQuestion from "../../components/forum/ask-question";
-import UploadForum from "../../components/forum/upload-forum";
 
 const Forums = () => {
   return (
     <div>
       <div className="container">
         <AskQuestion/>
-      </div>
-
-      <div className="container">
-        <UploadForum/>
       </div>
     </div>
 
