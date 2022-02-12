3de8519196d65efbfcb7b2b48d0679731d3ffca9
ebgyau <ebgyau@outlook.com>
2022-02-12 18:52:30 +0100

    added form to uplaod component

--- components/forum/upload-forum.tsx
+++ components/forum/upload-forum.tsx
@@ -1,12 +1,30 @@
+import { Button, Form, Input } from "antd";
 import React from "react";
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
 
 const UploadForum = () => {
     return (
-        <form >
-         <input type="text" placeholder="Enter your title" required/>
-         <textarea placeholder="Enter your description" required  />
-         <button>Submit Question</button>
-        </form>
+        <div>
+            <form {...layout} name="nest-messages">
+                <Form.Item label="Title" name="title" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item label="Description" name="description" rules={[{ required: true }]}>
+                    <Input.TextArea/>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                    <Button type="primary" htmlType="submit">
+                    Submit
+                    </Button>
+                </Form.Item>
+            </form>
+        </div>
     );
   };
   
--- pages/forums/index.tsx
+++ pages/forums/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import AskQuestion from "../../components/forum/ask-question";
+import UploadForum from "../../components/forum/upload-forum";
 
 const Forums = () => {
   return (
-    <div className="forums">
-      <AskQuestion/>
+    <div>
+      <div className="container">
+        <AskQuestion/>
+      </div>
+
+      <div className="container">
+        <UploadForum/>
+      </div>
     </div>
+
+       
   );
 };
 
